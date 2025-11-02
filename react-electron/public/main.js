b4e886d46b4c9b7fb398afa18a6c912fccc50323
IvaSv <148450331+IvaSv@users.noreply.github.com>
2025-11-02 15:24:38 +0100

    Test project from tutorial (file explorer)

--- react-electron/public/main.js
+++ react-electron/public/main.js
@@ -1,6 +1,8 @@
 const { app, BrowserWindow } = require("electron");
+const remoteMain = require("@electron/remote/main");
+const isDev = require("electron-is-dev");
 
-require("@electron/remote/main").initialize();
+remoteMain.initialize();
 
 function createWindow() {
   //create the browser window
@@ -8,21 +10,29 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
       enableRemoteModule: true,
     },
   });
 
-  win.loadURL("http://localhost:3000");
+  remoteMain.enable(win.webContents);
+
+  win.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
 }
 
 app.on("ready", createWindow);
 
 app.on("window-all-closed", function () {
   if (process.platform !== "darwin") {
-    app.quit;
+    app.quit();
   }
 });
 
 app.on("activate", function () {
-  if (BrowserWindow.getAllWindows.length === 0) createWindow();
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
